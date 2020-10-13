45807597b41dac27e32aac001f97246c6d1745ad
ssafaya <70345512+ssafaya@users.noreply.github.com>
2020-10-13 08:43:57 +0000

    commit data algo initial changes

---
+++ src/main/java/com/ssafaya/app/MainApp.java
@@ -0,0 +1,9 @@
+package com.ssafaya.app;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Main app started...");
+        System.out.println("This is a test program.");
+    }
+}
---
+++ src/test/java/com/ssafaya/app/Test.java
@@ -0,0 +1,12 @@
+package com.ssafaya.app;
+
+import org.junit.jupiter.api.Test;
+
+public class Test {
+    
+    @Test
+    public void test1() {
+        System.out.println("This is a test case.");
+        
+    }
+}
