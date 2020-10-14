7283b23bf305e07a26ff959f7d86ab396d9c92ef
ssafaya <Worship123#>
2020-10-14 09:02:23 +0100

    Commit fibonacci prog

---
+++ src/main/java/com/ssafaya/progs/FibonacciCalculator.java
@@ -0,0 +1,33 @@
+package com.ssafaya.progs;
+
+public class FibonacciCalculator {
+
+    private static final int inputNum = 30;
+    private static int stepCounter = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Starting fibonacci calculation...");
+        int result = calcFib(inputNum);
+        System.out.println("Fibonacci Calculation completed. Result : " + result);
+        System.out.println("Total Steps : " + stepCounter);
+    }
+
+    private static int calcFib(int num) {
+
+        System.out.println("Calc Fib for Num : " + num);
+        stepCounter++;
+        int result = 0;
+        if(num == 0) {
+            result = 0;
+        } else if(num == 1) {
+            result = 1;
+        } else if(num > 1) {
+            result = calcFib(num - 1) + calcFib(num - 2);
+        } else {
+            throw new RuntimeException("Invalid input passed. Input should be >= 0." + num);
+        }
+
+        System.out.println("Result for Fib num: " + num + " is: " + result);
+        return result;
+    }
+}
--- src/test/java/com/ssafaya/app/Test1.java
+++ src/test/java/com/ssafaya/app/Test1.java
@@ -2,9 +2,9 @@ package com.ssafaya.app;
 
 import org.junit.jupiter.api.Test;
 
-public class Test {
+public class Test1 {
     
-    @Test
+    //@Test
     public void test1() {
         System.out.println("This is a test case.");
         
